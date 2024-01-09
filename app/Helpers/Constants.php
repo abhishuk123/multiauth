42283ef15a04b62bf388dc6d73575dc7db74e427
abhishuk123 <abhishek.ku@infinitysoftsystems.com>
2024-01-09 21:28:48 +0530

    Initial commit

---
+++ app/Helpers/Constants.php
@@ -0,0 +1,16 @@
+<?php 
+ namespace App\Helpers;
+
+ class Constants
+ {
+
+    const USER = 0;
+    const ADMIN = 1;
+    const PROJECT_CORDINETER = 2;
+
+    const USER_TYPE =[
+        self::USER => 0,
+        self::ADMIN => 1,
+        self::PROJECT_CORDINETER =>2
+    ];
+ }
